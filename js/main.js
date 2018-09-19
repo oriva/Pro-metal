02962742a0d6f4457142b5257855f1f980faace8
Oriva <weelman93@gmail.com>
2018-09-19 22:51:57 +0800

    first

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$('.this-is-mebel button').click(function(){
+    $('.modal-form').addClass('show');
+});
+$('.go-modal').click(function(){
+    $('.modal-form').addClass('show');
+});
+
+$('.modal-form .closed').click(function(){
+    $('.modal-form').removeClass('show');
+});
+
+$(document).mousedown(function (e) {
+    var div = $(".modal-form .row");
+    if (!div.is(e.target)
+        && div.has(e.target).length === 0) {
+        $('html').removeClass('overfloff');
+        $('.modal-form').removeClass('show');
+    }
+});
+
+$('.scroll-weed-everyday').click(function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href')
+        , top = $(id).offset().top;
+    $('body,html').animate({
+        scrollTop: top
+    }, 1000);
+});
